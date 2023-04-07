42a1b025b570200ab548cb495e57b52be10fcff6
lucascmpus <lucascmpus@gmail.com>
2023-04-07 16:20:25 -0300

    feat: create product repository

---
+++ src/repository/PrismaProductRepository.ts
@@ -0,0 +1,63 @@
+import { Product } from 'src/entitys/product';
+import { ProductRepository } from './ProductRepository';
+import { PrismaClient } from '@prisma/client';
+
+export class PrismaProductRepository implements ProductRepository {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private readonly prisma: PrismaClient) { }
+
+  public async save(product: Product): Promise<void> {
+    await this.prisma.product.create({
+      data: {
+        name: product.name,
+        categoryId: product.category,
+        typeId: product.type,
+        value: product.value,
+      },
+    });
+  }
+
+  public async findByCategory(
+    category: number,
+  ): Promise<Product[] | undefined> {
+    const product = await this.prisma.product.findMany({
+      where: {
+        categoryId: category,
+      },
+    });
+
+    if (!product) return undefined;
+
+    // return new Product(product.id, product.name, product.caategory, product.type, product.values);
+
+    return product;
+  }
+
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        name,
+      },
+    });
+
+    if (!product) return undefined;
+
+    // return new Product(product.id, product.name, product.caategory, product.type, product.values);
+
+    return product;
+  }
+
+  public async findByType(type: number): Promise<Product[] | undefined> {
+    const product = await this.prisma.product.findMany({
+      where: {
+        typeId: type,
+      },
+    });
+
+    if (!product) return undefined;
+
+    // return new Product(product.id, product.name, product.caategory, product.type, product.values);
+
+    return product;
+  }
+}
--- src/repository/ProductRepository.ts
+++ src/repository/ProductRepository.ts
@@ -3,7 +3,6 @@ import { Product } from 'src/entitys/product';
 export interface ProductRepository {
   save(product: Product): Promise<void>;
   findByName(name: string): Promise<Product | undefined>;
-  findById(id: string): Promise<Product | undefined>;
-  findByCategory(category: string): Promise<Product[] | undefined>;
-  findByType(type: string): Promise<Product[] | undefined>;
+  findByCategory(category: number): Promise<Product[] | undefined>;
+  findByType(type: number): Promise<Product[] | undefined>;
 }
