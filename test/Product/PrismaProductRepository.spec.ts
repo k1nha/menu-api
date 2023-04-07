7985a51a1fd7957a7ec2dde97e27f6897d552ba4
lucascmpus <lucascmpus@gmail.com>
2023-04-07 16:35:31 -0300

    feat: test for create product

---
+++ test/Product/PrismaProductRepository.spec.ts
@@ -0,0 +1,32 @@
+import { PrismaProductRepository } from 'src/repository/PrismaProductRepository';
+import prisma from 'src/services/PrismaClient';
+
+const makeSut = () => {
+  return new PrismaProductRepository(prisma);
+};
+
+describe('Prisma Product Repository', () => {
+  beforeAll(async () => {
+    await prisma.product.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  test('Should create product', async () => {
+    const sut = makeSut();
+
+    await sut.save({
+      name: 'any name',
+      category: 2,
+      type: 1,
+      value: 1,
+    });
+
+    const product = await sut.findByName('any name');
+
+    expect(product).toHaveProperty('id');
+    expect(product?.name).toBe('any name');
+  });
+});
