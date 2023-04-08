16a6e5c58c8be909e61541dbcc915838bdc35a5d
lucascmpus <lucascmpus@gmail.com>
2023-04-08 18:23:06 -0300

    feat: finish all http methods

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -7,8 +7,39 @@ const makeRepository = () => {
 };
 
 export class ProductController {
+  async create(req: Request, res: Response) {
+    const body = req.body;
+
+    const rep = makeRepository().save(body);
+
+    return res.status(201).send(rep);
+  }
+
   async getAllProducts(req: Request, res: Response) {
     const rep = makeRepository().getAll;
+
+    return res.status(200).json({ data: rep });
+  }
+
+  async getProductByName(req: Request, res: Response) {
+    const { name } = req.params;
+
+    const rep = makeRepository().findByName(name);
+
+    return res.status(200).json({ data: rep });
+  }
+
+  async getProductByType(req: Request, res: Response) {
+    const typeId = +req.params;
+    const rep = makeRepository().findByType(typeId);
+
+    return res.status(200).json({ data: rep });
+  }
+
+  async getProductByCategory(req: Request, res: Response) {
+    const categoryId = +req.params;
+
+    const rep = makeRepository().findByCategory(categoryId);
 
     return res.status(200).json({ data: rep });
   }
