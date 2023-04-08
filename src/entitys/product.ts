72293af0fb3d316e4985a810c1e526fded48a7d3
lucascmpus <lucascmpus@gmail.com>
2023-04-08 18:21:41 -0300

    refactor: change properties name

--- src/entitys/product.ts
+++ src/entitys/product.ts
@@ -1,23 +1,23 @@
 interface ProductType {
-  // id: string;
+  id: string;
   name: string;
-  category: number;
-  type: number;
+  categoryId: number;
+  typeId: number;
   value: number;
 }
 
 export class Product {
-  // public readonly id: string;
+  public readonly id: string;
   public readonly name: string;
-  public readonly category: number;
-  public readonly type: number;
+  public readonly categoryId: number;
+  public readonly typeId: number;
   public readonly value: number;
 
-  constructor({ name, category, type, value }: ProductType) {
-    // this.id = id;
+  constructor({ id, name, categoryId, typeId, value }: ProductType) {
+    this.id = id;
     this.name = name;
-    this.category = category;
-    this.type = type;
+    this.categoryId = categoryId;
+    this.typeId = typeId;
     this.value = value;
   }
 }
