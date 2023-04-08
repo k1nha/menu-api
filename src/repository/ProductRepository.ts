228c0e91f3c8d412cd00e88d54c8a4d76c1800aa
lucascmpus <lucascmpus@gmail.com>
2023-04-08 18:22:05 -0300

    refactor: change promise return for products array

--- src/repository/ProductRepository.ts
+++ src/repository/ProductRepository.ts
@@ -3,7 +3,7 @@ import { Product } from 'src/entitys/product';
 export interface ProductRepository {
   save(product: Product): Promise<void>;
   getAll(): Promise<Product[]>;
-  findByName(name: string): Promise<Product | undefined>;
+  findByName(name: string): Promise<Product[] | undefined>;
   findByCategory(category: number): Promise<Product[] | undefined>;
   findByType(type: number): Promise<Product[] | undefined>;
 }
