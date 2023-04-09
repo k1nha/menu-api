100f362d8135eb2e8e4ab5b72e079355dd768d66
lucascmpus <lucascmpus@gmail.com>
2023-04-09 15:59:27 -0300

    feat: create category controller

---
+++ src/controllers/categoryController.ts
@@ -0,0 +1,31 @@
+import { Response, Request } from 'express';
+import { PrismaCategoryRepository } from 'src/repository/PrismaCategoryRepository';
+import prisma from 'src/services/PrismaClient';
+
+const makeRepository = () => {
+  return new PrismaCategoryRepository(prisma);
+};
+
+export class CategoryController {
+  async create(req: Request, res: Response) {
+    const body = req.body;
+
+    const rep = makeRepository().save(body);
+
+    return res.status(201).send(rep);
+  }
+
+  async getAll(req: Request, res: Response) {
+    const rep = makeRepository().getAll;
+
+    return res.status(200).json({ data: rep });
+  }
+
+  async delete(req: Request, res: Response) {
+    const categoryId = +req.params;
+
+    const rep = makeRepository().deleteUnique(categoryId);
+
+    return res.status(204).json({ data: rep });
+  }
+}
