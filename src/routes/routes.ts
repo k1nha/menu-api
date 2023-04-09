ac710a54abc50b3c17de4aaf83ee4e653d0b060a
lucascmpus <lucascmpus@gmail.com>
2023-04-09 16:00:48 -0300

    refactor: change routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,14 +4,22 @@ import { ProductController } from 'src/controllers/productController';
 const routes = Router();
 const product = new ProductController();
 
+// PRODUCTS
+// TODO: Delete and Put route for prduct
 routes.get('/product', product.getAllProducts);
-
 routes.post('/product/', product.create);
-
 routes.get('/product/category/:categoryId', product.getProductByCategory);
 routes.get('/product/:name', product.getProductByName);
 routes.get('/product/type/:typeId', product.getProductByType);
 
-// TODO: Delete and Put route for prduct
+// CATEGORIES
+routes.get('/product/category');
+routes.post('/products/category');
+routes.delete('/products/category/:categoryId');
+
+// TYPES
+routes.get('/product/type');
+routes.post('/products/type');
+routes.delete('/products/type/:typeId');
 
 export { routes };
